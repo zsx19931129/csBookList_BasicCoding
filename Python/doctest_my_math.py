d17da353c0d3413abbfb1ec40eefc2eac4d5cdb7
Shawn Zhang <zsx19931129@163.com>
2016-05-22 13:17:32 +0800

    doctest Demo

---
+++ Python/doctest_my_math.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+
+def square(x):
+	'''
+	Squares a number and returns the result.
+
+	>>> square(2)
+	4
+	>>> square(3)
+	9
+	'''
+	return x**x
+
+if __name__ == '__main__':
+	import doctest, doctest_my_math
+	doctest.testmod(doctest_my_math)
