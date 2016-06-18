332191a0ae28b7dd69b4258980c3f3de953e6ca1
Shawn Zhang <zsx19931129@163.com>
2016-06-18 19:43:15 +0800

    TWITTER命令客户端

---
+++ shellScripts/tweets.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# 文件名：tweets.sh
+# 用途：Twitter客户端基础
+
+USERNAME="PUT_USERNAME_HERE"
+PASSWORD="PUT_PASSWORD_HERE"
+COUNT="PUT_NO_OF_TWEETS"
+
+if [[ "$1" != "read" ]] && [[ "$1" != "tweet" ]];
+then
+    echo -e "Usage: $0 send status_message\n OR\n $0 read\n"
+    exit -1;
+fi
+
+if [[ "$1" == "read" ]];
+then
+    curl --silent -u $USERNAME:$PASSWORD http://twitter.com/statuses/friends_timeline.rss | \
+grep title | \
+tail -n +2 | \
+head -n $COUNT | \
+sed 's:.*<title>\([^<]*\).*:\n\1:'
+elif [[ "$1" = "tweet" ]];
+then
+    status=$( echo $@ | tr -d '"' | sed 's/.*tweet //')
+    curl --silent -u $USERNAME:$PASSWORD -d status="$status" http://twitter.com/statuses/update.xml > /dev/null
+    echo 'Tweeted :)'
+fi
+
