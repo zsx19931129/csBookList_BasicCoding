9bf7c4b63b85a6b6b4448da5666d4e32d56f0c70
Shawn Zhang <zsx19931129@163.com>
2016-06-20 21:49:36 +0800

    监视远程磁盘的监控情况

---
+++ shellScripts/disklog.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#文件名：disklog.sh
+#用途：监视远程系统的磁盘使用情况
+
+logfile="diskusage.log"
+if [[ -n $1 ]]
+then
+    logfile=$1
+fi
+if [ ! -e $logfile]
+then
+    printf "%-8s %-14s %-9s %-8s %-6s %-6s %-6s%s\n" "Date" "IP address" "Device" "Capacity" "Used" "Free" "Percent" "Status" > $logfile
+fi
+
+IP_LIST="127.0.0.1 0.0.0.0"
+#提供远程主机IP地址列表
+(
+for ip in $IP_LIST;
+do
+    ssh slyux@$ip 'df -H' | grep ^/dev/ > /tmp/$$.df
+
+    while read line;
+    do
+        cur_date=$(date +%D)
+        printf "%-8s %-14s" $cur_date $ip
+        echo $line | awk '{ printf("%-9s %-8s %-6s %-6s %-8s",$1,$2,$3,$4,$5;) }'
+        pusg=$(echo $line | egrep -o "[0-9]+%")
+        pusg=${pusg/\%/};
+        if [ $pusg -lt 80 ];
+        then 
+            echo SAFE
+        else
+            echo ALERT
+        fi
+    done < /tmp/$$.df
+done
+) >> $logfile
